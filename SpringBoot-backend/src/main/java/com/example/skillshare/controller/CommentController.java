19217eb186e47ecb40bdc75124dc00f03528909d
sarangalk <sarangakavindu03@gmail.com>
2025-05-07 23:24:57 +0530

    Edit comment controller part

--- SpringBoot-backend/src/main/java/com/example/skillshare/controller/CommentController.java
+++ SpringBoot-backend/src/main/java/com/example/skillshare/controller/CommentController.java
@@ -11,6 +11,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
+//comment controller part
 @RestController
 @RequestMapping("/api/comments")
 @RequiredArgsConstructor
