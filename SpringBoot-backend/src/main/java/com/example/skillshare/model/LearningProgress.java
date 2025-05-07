07ce6b5aab862d2eddc321b9b27970a30123f1e2
ravishka19 <ravishkabalasuriya@gmail.com>
2025-05-07 22:50:23 +0530

    fix2

--- SpringBoot-backend/src/main/java/com/example/skillshare/model/LearningProgress.java
+++ SpringBoot-backend/src/main/java/com/example/skillshare/model/LearningProgress.java
@@ -156,5 +156,11 @@ public class LearningProgress {
         this.userName = userName;
     }
 
+    public String getUserProfilePicture() {
+        return userProfilePicture;
+    }
 
+    public void setUserProfilePicture(String userProfilePicture) {
+        this.userProfilePicture = userProfilePicture;
+    }
 }
