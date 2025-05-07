8284e8fa0ef43e0822d606a846f8f720c63442ed
cpm055 <it22276896@my.sliit.lk>
2025-05-07 23:27:25 +0530

    Update learning plan

--- SpringBoot-backend/src/main/java/com/example/skillshare/controller/LearningPlanController.java
+++ SpringBoot-backend/src/main/java/com/example/skillshare/controller/LearningPlanController.java
@@ -56,17 +56,7 @@ public class LearningPlanController {
         return ResponseEntity.ok(learningPlan);
     }
 
-    @PutMapping("/{planId}")
-    public ResponseEntity<LearningPlan> updateLearningPlan(
-            @AuthenticationPrincipal UserDetails currentUser,
-            @PathVariable String planId,
-            @RequestBody LearningPlanDto learningPlanDto) {
-
-        LearningPlan learningPlan = learningPlanService.updateLearningPlan(currentUser.getUsername(), planId,
-                learningPlanDto);
-        return ResponseEntity.ok(learningPlan);
-    }
-
+    
     @DeleteMapping("/{planId}")
     public ResponseEntity<?> deleteLearningPlan(
             @AuthenticationPrincipal UserDetails currentUser,
