5b9571ef35960eae98bee3e6d67c9b8c0f2161e3
cpm055 <chathruwanpahansith055@gmail.com>
2025-05-16 14:27:07 +0530

    edit backend

--- SpringBoot-backend/src/main/java/com/example/skillshare/controller/CommentController.java
+++ SpringBoot-backend/src/main/java/com/example/skillshare/controller/CommentController.java
@@ -11,9 +11,6 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
-
-//backend
-//anvnan
 @RestController
 @RequestMapping("/api/comments")
 @RequiredArgsConstructor
